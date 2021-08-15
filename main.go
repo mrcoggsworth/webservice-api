2f1a735a013bb5622693fd1fb234a762c4bd18ac
Chris Scogin <mrcoggsworth@gmail.com>
2021-08-15 12:46:17 -0500

    add find product handler for GET and PUT

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type fooHandler struct {
@@ -78,6 +80,69 @@ func getNextID() int {
 	return highestId + 1
 }
 
+func findProductById(productId int) (*Product, int) {
+	for i, product := range productList {
+		if productId == product.ProductId {
+			return &product, i
+		}
+	}
+	return nil, 0
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	urlPathSegments := strings.Split(r.URL.Path, "products/")
+	productID, err := strconv.Atoi(urlPathSegments[len(urlPathSegments)-1])
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	product, listItemIndex := findProductById(productID)
+	if product == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+	// return a single product
+	case http.MethodGet:
+		productJSON, err := json.Marshal(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productJSON)
+	case http.MethodPut:
+		// update a product in the list
+		var updatedProduct Product
+		bs, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if err = json.Unmarshal(bs, &updatedProduct); err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if updatedProduct.ProductId != productID {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		product = &updatedProduct
+		productList[listItemIndex] = *product
+		w.WriteHeader(http.StatusOK)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+
+}
+
 func productsHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -145,6 +210,7 @@ func main() {
 	}
 
 	http.HandleFunc("/products", productsHandler)
+	http.HandleFunc("/products/", productHandler)
 	http.Handle("/foo", &fh)
 	http.HandleFunc("/bar", barHandler)
 
